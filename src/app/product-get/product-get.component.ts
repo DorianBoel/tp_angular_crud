064d1330d8ed04302a588935201f5d35c88e336b
DorianBoel <dorian.boel@free.fr>
2022-09-02 11:16:43 +0200

    add get product service & component

--- src/app/product-get/product-get.component.ts
+++ src/app/product-get/product-get.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-get',
-  templateUrl: './product-get.component.html',
-  styleUrls: ['./product-get.component.css']
+    selector: 'app-product-get',
+    templateUrl: './product-get.component.html',
+    styleUrls: ['./product-get.component.css']
 })
 export class ProductGetComponent implements OnInit {
 
-  constructor() { }
+    products: Product[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(private productService: ProductService) { }
+
+    ngOnInit(): void {
+        this.productService.getAllProducts().subscribe((data: Product) => {
+            this.products.push(data);
+        });
+    }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './model/product';
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +12,15 @@ export class ProductService {
 
     constructor(private http: HttpClient) { }
 
+    getAllProducts(): Observable<Product> {
+        return this.http.get<Product>(`${this.uri}`);
+    }
+
     addProduct(name: string, description: string, price: string) {
-        const obj = {
-            name, description, price
+        const obj: Record<string, string> = {
+            name,
+            description,
+            price
         };
         console.log(obj);
         this.http.post(`${this.uri}`, obj).subscribe(
